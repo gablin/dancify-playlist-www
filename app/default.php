df68716885bd9cfcda34615b7e8555628f4e00b1
Gabriel Hjort Blindell <gabriel@hjort.dev>
2020-01-30 14:09:40 -0500

    improve directory structure for insert-ding

--- app/default.php
+++ app/default.php
@@ -2,41 +2,5 @@
 require '../autoload.php';
 
 ensureSession();
-$session = getSession();
-$api = createWebApi($session);
-
-beginPage();
-try {
-?>
-
-<?php
-$playlists = loadPlaylists($api);
-?>
-
-<table>
-  <tr>
-    <th>Playlists</th>
-  </tr>
-  <?php
-  foreach ($playlists as $p) {
-    $name = $p->name;
-    $id = $p->id;
-    ?>
-    <tr>
-      <td>
-        <a href="/app/insert-ding/?playlist_id=<?php echo($id); ?>"><?php echo($name); ?></a>
-      </td>
-    </tr>
-    <?php
-  }
-  ?>
-</table>
-
-<?php
-}
-catch (Exception $e) {
-  showError($e);
-}
-endPage();
-updateTokens($session);
+header('Location: ./insert-ding/');
 ?>
