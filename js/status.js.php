8bdbf9ffef492c1596be69ccfca3815bd01d3a2e
Gabriel Hjort Åkerlund <gabriel@hjort.dev>
2022-01-29 11:28:04 -0500

    feat: Let failure messages stay in status for some time

--- js/status.js.php
+++ js/status.js.php
@@ -2,16 +2,36 @@
 require '../autoload.php';
 ?>
 
+const MS_FAILURE_LIMIT = 1000;
+var STATUS_FAILURE_REPORT_TIMESTAMP = 0;
+
+function getMsTimestamp() {
+  return Date.now();
+}
+
+function msElapsedSinceLastTimestamp() {
+  return getMsTimestamp() - STATUS_FAILURE_REPORT_TIMESTAMP;
+}
+
 function setStatus(s, indicate_failure = false) {
+  if (msElapsedSinceLastTimestamp() < MS_FAILURE_LIMIT && !indicate_failure) {
+    return;
+  }
+
   var status = $('.saving-status');
   status.text(s);
   status.removeClass('failed');
   if (indicate_failure) {
     status.addClass('failed');
+    STATUS_FAILURE_REPORT_TIMESTAMP = getMsTimestamp();
   }
 }
 
 function clearStatus() {
+  if (msElapsedSinceLastTimestamp() < MS_FAILURE_LIMIT) {
+    return;
+  }
+
   var status = $('.saving-status');
   status.empty();
   status.removeClass('failed');
