62a0e2ffc646220a153cb89518b6dd5a3907b591
Gabriel Hjort Åkerlund <gabriel@hjort.dev>
2022-04-05 15:37:01 -0400

    fix: Fix bug in sorting by genre

--- js/sort.js.php
+++ js/sort.js.php
@@ -26,14 +26,20 @@ function setupSort() {
 function sortTracks(table, direction, field) {
   var tracks = getTrackData(table);
   tracks = removePlaceholdersFromTracks(tracks);
+  function getGenre(t) {
+    if (t.genre.by_user == 0 && t.genre.by_others.length > 0) {
+      return t.genre.by_others[0];
+    }
+    return t.genre.by_user;
+  }
   function cmp(a, b) {
     var res = 0;
     if (field == 'bpm') {
       res = intcmp(a.bpm, b.bpm);
     }
     else if (field == 'genre') {
-      s1 = genreToString(a.genre);
-      s2 = genreToString(b.genre);
+      s1 = genreToString(getGenre(a));
+      s2 = genreToString(getGenre(b));
       res = strcmp(s1, s2);
     }
     return res * direction;
