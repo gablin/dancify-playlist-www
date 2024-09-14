93a378158308ab3d2454310d520a19b721ba5604
Gabriel Hjort Åkerlund <gabriel@hjort.dev>
2024-09-14 23:18:06 +0200

    Add minizinc binaries

--- api/randomize-by-bpm/default.php
+++ api/randomize-by-bpm/default.php
@@ -127,7 +127,7 @@ if (fwrite($fh, $dzn_content) === false) {
 // Solve model and get output
 $time_limit_s = 60;
 $time_limit_ms = $time_limit_s*1000;
-$res = shell_exec( "minizinc model.mzn $dzn_file " .
+$res = shell_exec( "./minizinc/bin/minizinc model.mzn $dzn_file " .
                    "--time-limit $time_limit_ms " .
                    "--unbounded-msg '' " .
                    "--unknown-msg '' " .
@@ -138,7 +138,7 @@ $res = shell_exec( "minizinc model.mzn $dzn_file " .
 $json = fromJson($res);
 if (is_null($json)) {
   $clean_up_fun();
-  fail("no solution found");
+  fail("no solution found: $dir");
 }
 
 // Build new track list
