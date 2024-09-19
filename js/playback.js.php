dee04490207915bfa04d28a39db1644acb1b20ca
Gabriel Hjort Åkerlund <gabriel@hjort.dev>
2024-09-19 22:15:49 +0200

    Add next and prev button to playback control

--- js/playback.js.php
+++ js/playback.js.php
@@ -81,22 +81,28 @@ function mkPlaybackHtml() {
   let playback_html =
     $( '<div class="playback">' +
        '  <div class="x-container">' +
-       '    <button class="controller"></button>' +
        '    <div class="playing">' +
        '      <div class="name">' +
        '        <?= LNG_DESC_DOUBLE_CLICK_TRACK_TO_PLAY ?>' +
        '      </div>' +
        '      <div class="artists"></div>' +
        '    </div>' +
-       '    <div class="seek">' +
-       '      <div class="length-pos"></div>' +
-       '      <div class="bar-wrapper">' +
-       '        <div class="bar">' +
-       '          <div class="active-bar"></div>' +
-       '          <div class="knob"></div>' +
-       '        </div>' +
+       '    <div class="middle">' +
+       '      <div class="controllers">' +
+       '        <button class="prev" />' +
+       '        <button class="play" />' +
+       '        <button class="next" />' +
        '      </div>' +
-       '      <div class="length-end"></div>' +
+       '      <div class="seek">' +
+       '        <div class="length-pos"></div>' +
+       '        <div class="bar-wrapper">' +
+       '          <div class="bar">' +
+       '            <div class="active-bar"></div>' +
+       '            <div class="knob"></div>' +
+       '          </div>' +
+       '        </div>' +
+       '        <div class="length-end"></div>' +
+       '      </div>' +
        '    </div>' +
        '    <div class="volume">' +
        '      <div class="bar-wrapper">' +
@@ -110,7 +116,7 @@ function mkPlaybackHtml() {
        '</div>'
      );
   $('.footer').before(playback_html);
-  getPlaybackButton().click(togglePlay);
+  setupControllerButtons();
   setupSeekController();
   setupVolumeController();
   renderPaused();
@@ -120,24 +126,47 @@ function getPlaybackArea() {
   return $('.playback');
 }
 
-function getPlaybackButton() {
-  return getPlaybackArea().find('button.controller');
+function getPlayButton() {
+  return getPlaybackArea().find('.controllers button.play');
+}
+
+function setupPrevButton() {
+  let button = getPlaybackArea().find('.controllers button.prev');
+  button.html(
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 24">' +
+    '  <path fill="currentColor" d="M 22 24 L 22 0 L 2 12 L 20 24 Z"/>' +
+    '  <path fill="currentColor" d="M 0 24 L 0 0 L 2 0 L 2 24 Z"/>' +
+    '</svg>'
+  );
+  button.click(rewindOrPlayPrevTrack);
+}
+
+function setupNextButton() {
+  let button = getPlaybackArea().find('.controllers button.next');
+  button.html(
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 24">' +
+    '  <path fill="currentColor" d="M 0 24 L 0 0 L 20 12 L 0 24 Z"/>' +
+    '  <path fill="currentColor" d="M 22 24 L 22 0 L 20 0 L 20 24 Z"/>' +
+    '</svg>'
+  );
+  button.click(playNextTrack);
 }
 
 function renderPlaying() {
-  getPlaybackButton().html(
-    '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"' +
-    '    viewBox="0 -6 24 26">' +
-    '  <path fill="currentColor" d="M10 24h-6v-24h6v24zm10-24h-6v24h6v-24z"/>' +
+  getPlayButton().html(
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 24">' +
+    '  <path fill="currentColor" ' +
+    '    d="M 6 24 L 0 24 L 0 0 L 6 0 L 6 24 Z"/>' +
+    '  <path fill="currentColor" ' +
+    '    d="M 18 0 L 12 0 L 12 24 L 18 24 L 18 0 Z"/>' +
     '</svg>'
   );
 }
 
 function renderPaused() {
-  getPlaybackButton().html(
-    '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"' +
-    '    viewBox="-2 -6 24 30">' +
-    '  <path fill="currentColor" d="M2 24v-24l20 12-20 12z"/>' +
+  getPlayButton().html(
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 24" class="triangle">' +
+    '  <path fill="currentColor" d="M 0 24 L 0 0 L 20 12 L 0 24 Z"/>' +
     '</svg>'
   );
 }
@@ -286,53 +315,7 @@ function checkPlayPos() {
            state.position > adjustDuration(state.duration)
          ) {
         stopSeek();
-
-        const playlist_table = getPlaylistTable();
-        const local_scratchpad_table = getLocalScratchpadTable();
-        const global_scratchpad_table = getGlobalScratchpadTable();
-        const track_data =
-          [ [ playlist_table
-            , removePlaceholdersFromTracks(getTrackData(playlist_table))
-            ]
-          , [ local_scratchpad_table
-            , removePlaceholdersFromTracks(getTrackData(local_scratchpad_table))
-            ]
-          , [ global_scratchpad_table
-            , removePlaceholdersFromTracks(getTrackData(global_scratchpad_table))
-            ]
-          ];
-        for (let i = 0; i < track_data.length; i++) {
-          const table = track_data[i][0];
-          const tracks = track_data[i][1];
-
-          // Find best index to consider as last played track
-          let best_playing_index = -1;
-          let best_playing_index_diff = Number.MAX_SAFE_INTEGER;
-          for (let j = 0; j < tracks.length; j++) {
-            let t = tracks[j];
-            let diff = Math.abs(MARKED_AS_PLAYING_INDEX - j);
-            if ( table.is(MARKED_AS_PLAYING_TABLE) &&
-                 t.trackId === PLAYBACK_LAST_PLAYED_TRACK_ID &&
-                 diff < best_playing_index_diff
-               ) {
-              best_playing_index = j;
-              best_playing_index_diff = diff;
-            }
-          }
-
-          if (best_playing_index >= 0) {
-            let next_j = best_playing_index + 1;
-            if (next_j < tracks.length) {
-              PLAYBACK_LAST_PLAYED_INDEX = next_j;
-              playTrack( tracks[next_j].trackId
-                       , PLAYBACK_LAST_PLAYED_INDEX
-                       , PLAYBACK_LAST_PLAYED_TABLE
-                       , 0
-                       );
-              return;
-            }
-          }
-        }
+        playNextTrack();
       }
     }
   );
@@ -363,6 +346,131 @@ function togglePlay() {
       }
     }
   );
+}
+
+function getBestPlayingTrackWindow() {
+  const playlist_table = getPlaylistTable();
+  const local_scratchpad_table = getLocalScratchpadTable();
+  const global_scratchpad_table = getGlobalScratchpadTable();
+  const track_data =
+    [ [ playlist_table
+      , removePlaceholdersFromTracks(getTrackData(playlist_table))
+      ]
+    , [ local_scratchpad_table
+      , removePlaceholdersFromTracks(getTrackData(local_scratchpad_table))
+      ]
+    , [ global_scratchpad_table
+      , removePlaceholdersFromTracks(getTrackData(global_scratchpad_table))
+      ]
+    ];
+  for (let i = 0; i < track_data.length; i++) {
+    const table = track_data[i][0];
+    const tracks = track_data[i][1];
+
+    // Find best index to consider as last played track
+    let best_playing_index = -1;
+    let best_playing_index_diff = Number.MAX_SAFE_INTEGER;
+    for (let j = 0; j < tracks.length; j++) {
+      let t = tracks[j];
+      let diff = Math.abs(MARKED_AS_PLAYING_INDEX - j);
+      if ( table.is(MARKED_AS_PLAYING_TABLE) &&
+           t.trackId === PLAYBACK_LAST_PLAYED_TRACK_ID &&
+           diff < best_playing_index_diff
+         ) {
+        best_playing_index = j;
+        best_playing_index_diff = diff;
+      }
+    }
+
+    if (best_playing_index >= 0) {
+      let idx = best_playing_index;
+      let current = [idx, tracks[idx].trackId];
+      let prev = idx > 0 ? [idx-1, tracks[idx-1].trackId] : [null, null];
+      let next = idx < tracks.length-1 ? [idx+1, tracks[idx+1].trackId]
+                                       : [null, null];
+      return [prev, current, next];
+    }
+  }
+
+  return [null, null, null];
+}
+
+function rewindOrPlayPrevTrack() {
+  if (!PLAYBACK_PLAYER) {
+    return;
+  }
+
+  PLAYBACK_PLAYER.activateElement();
+
+  function playPrevTrack() {
+    let [[prev_i, prev_track_id], current, next] = getBestPlayingTrackWindow();
+    if (prev_track_id) {
+      playTrack( prev_track_id
+               , prev_i
+               , PLAYBACK_LAST_PLAYED_TABLE
+               , 0
+               );
+    }
+  }
+
+  PLAYBACK_PLAYER.getCurrentState().then(
+    state => {
+      if (!state) {
+        playPrevTrack();
+        return;
+      }
+
+      if (state.position < 1000) {
+        playPrevTrack();
+      }
+      else {
+        playTrack( PLAYBACK_LAST_PLAYED_TRACK_ID
+                 , PLAYBACK_LAST_PLAYED_INDEX
+                 , PLAYBACK_LAST_PLAYED_TABLE
+                 , 0
+                 );
+      }
+    }
+  );
+}
+
+function playNextTrack() {
+  if (!PLAYBACK_PLAYER) {
+    return;
+  }
+
+  if (PLAYBACK_IS_STOPPED) {
+    return
+  }
+
+  PLAYBACK_PLAYER.activateElement();
+
+  let [prev, current, [next_i, next_track_id]] = getBestPlayingTrackWindow();
+
+  if (next_track_id) {
+    playTrack( next_track_id
+             , next_i
+             , PLAYBACK_LAST_PLAYED_TABLE
+             , 0
+             );
+  }
+  else {
+    // This is the last track; skip to end of it
+    PLAYBACK_IS_STOPPED = true;
+    PLAYBACK_PLAYER.getCurrentState().then(
+      state => {
+        if (!state) return;
+        // Must skip to almost end of track, or else it will try to play it from
+        // the beginning.
+        let end_pos = adjustDuration(state.duration) - 200;
+        playTrack( PLAYBACK_LAST_PLAYED_TRACK_ID
+                 , PLAYBACK_LAST_PLAYED_INDEX
+                 , PLAYBACK_LAST_PLAYED_TABLE
+                 , end_pos
+                 );
+      }
+    );
+  }
 }
 
 function triggerResume() {
@@ -406,7 +514,12 @@ function playTrack( track_id
                   , table
                   , pos_ms
                   , success_f = function() {}
-                  , fail_f = showPlaybackError
+                  , fail_f = function() {
+                      showPlaybackError(
+                        '<?= LNG_ERR_PLAYBACK_TRACK_COULD_NOT_PLAY ?>'
+                      );
+                      startSeek(); // Restore seek
+                    }
                   , attempts = PLAYBACK_PLAY_TRACK_ATTEMPTS
                   ) {
   if (!PLAYBACK_PLAYER) {
@@ -487,6 +600,12 @@ function renderTrackInfo(name, artists) {
   area.find('.playing .artists').text(artists !== null ? artists.join(', ') : '');
 }
 
+function setupControllerButtons() {
+  setupPrevButton();
+  setupNextButton();
+  getPlayButton().click(togglePlay);
+}
+
 function setupSeekController() {
   let seek_area = getPlaybackArea().find('.seek');
   let knob = seek_area.find('.knob');
@@ -539,13 +658,6 @@ function setupSeekController() {
                      , PLAYBACK_LAST_PLAYED_INDEX
                      , PLAYBACK_LAST_PLAYED_TABLE
                      , position_ms
-                     , function() {}
-                     , function() {
-                         showPlaybackError(
-                           '<?= LNG_ERR_PLAYBACK_TRACK_COULD_NOT_PLAY ?>'
-                         );
-                         startSeek(); // Restore seek
-                       }
                      );
           }
         );
